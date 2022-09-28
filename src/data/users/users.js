bb36a3d0db18b0e884291dff3660652dc66a1f0e
Ikram <ikramzulfugar@gmail.com>
2022-09-29 02:41:47 +0400

    Unnecessarry folders cleared

---
+++ src/data/users/users.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    username: "Nargiz",
+    password: "coastical1837",
+  },
+  {
+    username: "Farid",
+    password: "heyygetit5939",
+  },
+  {
+    username: "Laman",
+    password: "tunintus3481",
+  },
+  {
+    username: "Akif",
+    password: "eraifkal2170",
+  },
+  {
+    username: "Nurlan",
+    password: "woanofk2841",
+  },
+];
